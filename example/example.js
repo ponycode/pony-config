0ee59e3df66c03d12d375cebbae3d79a4763c4f3
scott <scott@ponycode.com>
2014-11-03 13:47:04 -0800

    example demonstrate chaining and get/set

--- example/example.js
+++ example/example.js
@@ -2,9 +2,26 @@
 
 
 var config = require('../index.js');
-config.options = { debug: true };
-config.environmentSearch( { paths:['./example-env-file'], env: 'ENVIRONMENT', default:'prod', debug: true} );
 
-config.list();
+console.log('');
+console.log( 'Demonstrating loading configuration');
 
+config
+    .setOptions( { debug: true } )
+    .environmentSearch( { paths:['./example-env-file'], env: 'ENVIRONMENT', default:'prod', debug: true} )
+    .useObject( {'organization': 'PonyCode' } )
+    .useFile( 'example-config.json', ['dev'] )
+    .list();
+
+console.log('');
+console.log( 'Demonstrating reading and writing configuration ');
+
+console.log('Initial State:', config.get('address.state', 'unset') );
+config.set('address.state', 'TX' );
+console.log('New State:', config.get('address.state', 'unset') );
+
+console.log('');
+console.log('New Address:', config.get('address') );
+
+console.log('');
 console.log('Done.');
