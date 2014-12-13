50e2eb583b9d47ad8405b26876877b27f034cdcf
scott <scott@ponycode.com>
2014-12-13 14:38:32 -0800

    object merge method

---
+++ test/test-obj.js
@@ -0,0 +1,157 @@
+var assert = require("assert");
+
+var objMerge = require('../lib/object-merge');
+
+describe('objMerge', function(){
+    it('should return an object', function(){
+        assert.equal( typeof objMerge, 'object');
+    });
+});
+
+describe('Merge Primitives', function(){
+    it('should return a number', function(){
+        assert.equal( objMerge.merge( 2, undefined ), 2);
+    });
+
+    it('should return a number', function(){
+        assert.equal( objMerge.merge( undefined, 2 ), 2);
+    });
+
+    it('should return a number', function(){
+        assert.equal( objMerge.merge( 4, 2 ), 2);
+    });
+
+    it('should return a string', function(){
+        assert.equal( objMerge.merge( 4, "house" ), "house");
+    });
+
+    it('should return a string', function(){
+        assert.equal( objMerge.merge( "pickle", "house" ), "house");
+    });
+
+    it('should return a boolean', function(){
+        assert.equal( objMerge.merge( false, true ), true);
+    });
+
+    it('should return a boolean', function(){
+        assert.equal( objMerge.merge( true, false ), false);
+    });
+});
+
+describe('Merge Primitive and Empty Object', function() {
+    it('should return a boolean', function(){
+        assert.equal( objMerge.merge( {}, true ), true);
+    });
+
+    it('should return an object', function(){
+        assert.deepEqual( objMerge.merge( true, {} ), {});
+    });
+});
+
+describe('Merge Primitive and Object', function() {
+    it('should return a boolean', function(){
+        assert.equal( objMerge.merge( { key : "value" }, true ), true);
+    });
+
+    it('should return an object', function(){
+        assert.deepEqual( objMerge.merge( true, { key : "value" } ), { key : "value" });
+    });
+});
+
+describe('Merge Objects 1 level deep', function() {
+    it('should merge keys into new object', function(){
+        assert.deepEqual( objMerge.merge( { key1 : "value1" }, { key2 : "value2" } ), { key1 : "value1", key2 : "value2" });
+    });
+
+    it('should merge object over another', function(){
+        assert.deepEqual( objMerge.merge( { key1 : "value1" }, { key1 : "value2" } ), { key1 : "value2" });
+    });
+});
+
+describe('Merge Objects 2 levels deep', function() {
+
+    it('should merge object over primitive property', function(){
+        assert.deepEqual( objMerge.merge( { key1 : "value1" }, { key1 : { key2 : "value2" }} ), { key1 : { key2 : "value2" }});
+    });
+
+    it('should merge primitive property over object', function(){
+        assert.deepEqual( objMerge.merge( { key1 : { key2 : "value2" }},  { key1 : "value1" } ), { key1 : "value1" });
+    });
+
+    it('should replace primitive with deep object', function(){
+        assert.deepEqual( objMerge.merge( 4, { key1 : { key2 : "value2" }} ), { key1 : { key2 : "value2" }});
+    });
+
+    it('should replace deep object with primitive', function(){
+        assert.deepEqual( objMerge.merge( { key1 : { key2 : "value2" }}, 4 ), 4);
+    });
+
+    it('should merge properties in object 2 levels deep', function(){
+        assert.deepEqual( objMerge.merge( { key1 : { key2 : "value2" }}, { key1 : { key3 : "value3" }} ), { key1 : { key2 : "value2", key3 : "value3" }});
+    });
+});
+
+describe('Original objects remain unchanged', function() {
+
+    it('should merge object over primitive property', function(){
+
+        var inputObject1 = {
+            key1 : "value1",
+            key2 : {
+                key2a : "value2a",
+                key2b : "value2b"
+            },
+            key3 : {
+                key3a : {
+                    key3a1: "value3a1"
+                }
+            }
+        };
+
+        var inputObject2 = {
+            key1 : "value1`",               // key1 gets new value
+            key2 : {
+                key2c : "value2c`"          // key 2 news property
+            },
+            key3 : "value3`"                // key3 gets replaced with a primitive
+        };
+
+        var outputObject = objMerge.merge( inputObject1, inputObject2 );
+
+        assert.deepEqual( outputObject,
+            {
+                key1 : "value1`",
+                key2 : {
+                    key2a : "value2a",
+                    key2b : "value2b",
+                    key2c : "value2c`"
+                },
+                key3 : "value3`"                // key3 gets replaced with a primitive
+            });
+
+        assert.deepEqual( inputObject1,
+            {
+                key1 : "value1",
+                key2 : {
+                    key2a : "value2a",
+                    key2b : "value2b"
+                },
+                key3 : {
+                    key3a : {
+                        key3a1: "value3a1"
+                    }
+                }
+            });
+
+        assert.deepEqual( inputObject2,
+            {
+                key1 : "value1`",               // key1 gets new value
+                key2 : {
+                    key2c : "value2c`"          // key 2 news property
+                },
+                key3 : "value3`"                // key3 gets replaced with a primitive
+            });
+    });
+
+});
+
