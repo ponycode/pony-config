1d3c57299c442450bb9376fbd2144198b4a777d0
scott <scott@ponycode.com>
2015-02-28 13:31:23 -0800

    cleanup example

--- examples/example_4.js
+++ examples/example_4.js
@@ -1,21 +1,19 @@
 var config = require('../index.js');
 
-config.setOptions( { debug: true, immutable: true } );
+config.setOptions( { debug: true } );
+
 config.findEnvironment( { paths:['~/test-pony-config-env','./example-env-file'], env: 'ENVIRONMENT', default:'prod', debug: true} );
 
 config.useObject({ 'organization': 'PonyCode' });
 config.when(['dev']).useFile( 'example-dev-config.json' );
 config.when(['prod','stage']).useFile( 'example-prod-config.json' );
-config.when('dev').useCommandLineArguments({ path:'address.zip', options:'zip' });
+config.when('dev').useCommandLineArguments({ path:'address.zip', options:['zip','zipcode'] });
 
 config.set('address.zip-state', config.get('address.zip')+'-'+config.get('address.state'));
 config.set('occupants', ['Scott','Steve','Stacy', ["Mother","Father"]] );
 config.set('token', new Buffer("secretToken") );
 config.set('date', new Date() );
-
-config.get('address.zip');
-
-config.lock( false );
+config.set('fn', function( x ){ return x+1; } );
 
 config.list();
 
